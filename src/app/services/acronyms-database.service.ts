34a4359602b70acf2916d6b3496f2dc04c5ad0b5
Maxim Filimonov <max.filimonov@gmail.com>
2020-07-23 15:46:27 +0100

    small change to baseurl

--- src/app/services/acronyms-database.service.ts
+++ src/app/services/acronyms-database.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 
-//const baseUrl = 'http://localhost:8083/';
 const baseUrl = environment.baseApiUrl;
 //const baseUrl = '/';
 
