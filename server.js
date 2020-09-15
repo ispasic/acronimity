33391f8c93c8890cf6470693507dab668d69e17a
Maxim Filimonov <max.filimonov@gmail.com>
2020-09-15 14:09:32 +0100

    promisified connection for async/await

--- server.js
+++ server.js
@@ -2,18 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const util = require( 'util' );
 const events = require('./backend/events');
 
-const connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'acronyms',
-        password: '-3GN^35-Ft8w?6wc',
-        database: 'acronyms'
-    }
-);
+dbConfig = 
+{
+    host: 'localhost',
+    user: 'acronyms',
+    password: '-3GN^35-Ft8w?6wc',
+    database: 'acronyms'
+};
 
-connection.connect();
+//ordinary connection to mysql for callbacks only
+//const connection = mysql.createConnection(dbConfig);
+//connection.connect();
+
+//promisified connection to mysql for async/await
+function makeDb( config ) {
+  const connection = mysql.createConnection( config );
+  return {
+    query( sql, args ) {
+      return util.promisify( connection.query )
+        .call( connection, sql, args );
+    },
+    close() {
+      return util.promisify( connection.end ).call( connection );
+    }
+  };
+}
+const connection = makeDb( dbConfig );
+
 
 const port = process.env.PORT || 8083;
 
