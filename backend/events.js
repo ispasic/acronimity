5c429f4c4f58db46744491cc25034d123c8d09cc
Maxim Filimonov <max.filimonov@gmail.com>
2020-09-15 15:07:48 +0100

    async await for post and get api calls

--- backend/events.js
+++ backend/events.js
@@ -6,70 +6,17 @@ function createRouter(db) {
 
     //router will be here
 
-    router.post('/insertAcronym1', (req, res, next) => {
-        //check if empty request
-        if (!req.body.longform || !req.body.shortform)
-        {
-            let msg = 'Empty shortform or longform';
-            res.status(500).json({
-                status: 'ok',
-                message: msg
-            });
-            return;
-        }
-
-        let shortform = req.body.shortform;
-        let longform = req.body.longform;
-
-        //check if shortform is already in database
-        db.query(
-            'SELECT shortform, longform FROM lexicon WHERE shortform=?',
-            [shortform],
-            (error, results) => {
-                if (error) {
-                    console.error(error);
-                    res.status(500).json({status: 'error'});
-                } else if (results.length != 0) {
-                    let msg = 'this shortform is already in the database';
-                    res.status(500).json({
-                        status: 'error',
-                        message: msg
-                    });
-                } else { //valid acronym for insertion
-                    db.query(
-                        'INSERT INTO lexicon (shortform, longform) VALUES (?,?)',
-                        [shortform, longform],
-                        (error) => {
-                            if (error) {
-                                console.log(shortform);
-                                console.error(error);
-                                res.status(500).json({status: 'error'});
-                            } else {
-                                let msg = `inserted new acronym with shortform: ${shortform} and longform: ${longform}`;
-                                res.status(200).json({
-                                    status: 'ok',
-                                    message: msg
-                                });
-                            }
-                        }
-                    );
-                }
-            }
-        );
-    });
-
     router.post('/insertAcronym', async (req, res, next) => {
-        
+
         //check if empty request
         if (!req.body.longform || !req.body.shortform || !req.body.text ||
             !req.body.swapText || !req.body.tagText || !req.body.pubMedId)
         {
             let msg = 'Not enough parameters in the request body';
             res.status(500).json({
-                status: 'ok',
+                status: 'error',
                 message: msg
             });
-            return;
         }
 
         let shortform = req.body.shortform;
@@ -131,6 +78,135 @@ function createRouter(db) {
         }
     });
 
+    router.get('/getAcronymByShort', async function(req, res, next) {
+
+        //check if empty request
+        if (!req.body.shortform)
+        {
+            let msg = 'Empty shortform';
+            res.status(500).json({
+                status: 'error',
+                message: msg
+            });
+        }
+        let shortform = req.body.shortform;
+
+        try {
+            const result = await db.query(
+                'SELECT * FROM lexicon INNER JOIN abstract ON lexicon.abstract_id=abstract.id WHERE shortform=?',
+                [shortform]
+            )
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+    router.get('/getAcronymByLong', async function(req, res, next) {
+
+        //check if empty request
+        if (!req.body.longform)
+        {
+            let msg = 'Empty longform';
+            res.status(500).json({
+                status: 'error',
+                message: msg
+            });
+        }
+        let longform = req.body.longform;
+
+        try {
+            const result = await db.query(
+                'SELECT * FROM lexicon INNER JOIN abstract ON lexicon.abstract_id=abstract.id WHERE longform=?',
+                [longform]
+            )
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+    router.get('/getAllAcronyms', async (req, res, next) => {
+        try {
+            const results = await db.query(
+                'SELECT * FROM lexicon INNER JOIN abstract ON lexicon.abstract_id=abstract.id',
+            )
+            res.status(200).json(results);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+
+
+    //old implementation with callbacks
+
+
+    router.post('/insertAcronymOld', (req, res, next) => {
+        //check if empty request
+        if (!req.body.longform || !req.body.shortform)
+        {
+            let msg = 'Empty shortform or longform';
+            res.status(500).json({
+                status: 'ok',
+                message: msg
+            });
+            return;
+        }
+
+        let shortform = req.body.shortform;
+        let longform = req.body.longform;
+
+        //check if shortform is already in database
+        db.query(
+            'SELECT shortform, longform FROM lexicon WHERE shortform=?',
+            [shortform],
+            (error, results) => {
+                if (error) {
+                    console.error(error);
+                    res.status(500).json({status: 'error'});
+                } else if (results.length != 0) {
+                    let msg = 'this shortform is already in the database';
+                    res.status(500).json({
+                        status: 'error',
+                        message: msg
+                    });
+                } else { //valid acronym for insertion
+                    db.query(
+                        'INSERT INTO lexicon (shortform, longform) VALUES (?,?)',
+                        [shortform, longform],
+                        (error) => {
+                            if (error) {
+                                console.log(shortform);
+                                console.error(error);
+                                res.status(500).json({status: 'error'});
+                            } else {
+                                let msg = `inserted new acronym with shortform: ${shortform} and longform: ${longform}`;
+                                res.status(200).json({
+                                    status: 'ok',
+                                    message: msg
+                                });
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    });
+
+    router.get('/getAllAcronymsOld', (req, res, next) => {
+        db.query(
+            'SELECT * FROM lexicon',
+            (error, results) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).json({status: 'error'});
+                } else {
+                    res.status(200).json(results);
+                }
+            }
+        );
+    });
+
     router.post('/updateAcronym', (req, res, next) => {
 
         //check if empty request
@@ -184,7 +260,7 @@ function createRouter(db) {
         );     
     });
 
-    router.get('/getAcronymByShort', function(req, res, next) {
+    router.get('/getAcronymByShortOld', function(req, res, next) {
 
         //check if empty request
         if (!req.body.shortform)
@@ -218,7 +294,7 @@ function createRouter(db) {
         );
     });
 
-    router.get('/getAcronymByLong', function(req, res, next) {
+    router.get('/getAcronymByLongOld', function(req, res, next) {
         
         //check if empty request
         if (!req.body.longform)
@@ -245,20 +321,6 @@ function createRouter(db) {
                         status: 'ok',
                         message: msg
                     });
-                } else {
-                    res.status(200).json(results);
-                }
-            }
-        );
-    });
-
-    router.get('/getAllAcronyms', (req, res, next) => {
-        db.query(
-            'SELECT * FROM lexicon',
-            (error, results) => {
-                if(error) {
-                    console.log(error);
-                    res.status(500).json({status: 'error'});
                 } else {
                     res.status(200).json(results);
                 }
