6b1b0c446020f081cf6505be4b4836f7a34f79e3
Maxim Filimonov <max.filimonov@gmail.com>
2021-01-18 14:36:07 +0000

    regex refine for tagging

--- src/app/services/abstract-processing.service.ts
+++ src/app/services/abstract-processing.service.ts
@@ -7,6 +7,51 @@ export class AbstractProcessingService {
 
   constructor() { }
 
+  public tagAcronymsSense(text: string, acronymList: any[]): string {
+    
+    let taggedText = text;
+
+    taggedText = taggedText.replace(/\n/g, " "); //swap all endlines by spaces
+    taggedText = taggedText.replace(/\u00A0/g, " "); //swap all non-breaking spaces by spaces
+    taggedText = taggedText.replace(/\s{2,}/g,' '); //swap all multiple spaces with spaces
+
+    for (let acronym of acronymList) {
+      // transform `longform (shortform)` strings into `shortform`
+      taggedText = this.replaceAll(taggedText, acronym.longform + " (" + acronym.shortform + ")", acronym.shortform);
+      taggedText = this.replaceAll(taggedText, acronym.longform + "(" + acronym.shortform + ")", acronym.shortform);
+      // transform `shortform (longform)` string into `shortform`
+      taggedText = this.replaceAll(taggedText, acronym.shortform + " (" + acronym.longform + ")", acronym.shortform);
+      taggedText = this.replaceAll(taggedText, acronym.shortform + "(" + acronym.longform + ")", acronym.shortform);
+      // transform `(shortform) longform` string into `shortform`
+      taggedText = this.replaceAll(taggedText, "(" + acronym.shortform + ") " + acronym.longform, acronym.shortform);
+      taggedText = this.replaceAll(taggedText, "(" + acronym.shortform + ")" + acronym.longform, acronym.shortform);
+      // transform `shortform` into `<acronym sense=longform>shortform</acronym>
+      taggedText = this.replaceAllBoundaries(taggedText, acronym.shortform, " <acronym sense='" + acronym.longform + "'>" + acronym.shortform + "</acronym> ");
+    }
+
+    return taggedText;
+  }
+
+
+  //replace all occurences of <find> with <replace> in <str>
+  public replaceAllBoundaries(str, find: string, replace: string) {
+    console.log(`Replacing ${find} with ${replace}`);
+    return str.replace(new RegExp(`[\.\\s]${find}[\.\\s]`, 'gi'), replace);
+  }
+
+  private replaceAll(str, term, replacement) {
+    return str.replace(new RegExp(this.escapeRegExp(term), 'gi'), replacement);
+  } 
+
+  private escapeRegExp(string){
+    let result = string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    return result;
+  }
+
+
+
+  // old code
+
   //function for swapping acronyms' longform and shortform in text
   public swapAcronyms(text: string, acronymList: any[]): string {
 
@@ -49,7 +94,7 @@ export class AbstractProcessingService {
     return taggedText;
   }
 
-  public tagAcronymsSense(text: string, acronymList: any[]): string {
+  public tagAcronymsSenseOld(text: string, acronymList: any[]): string {
     
     let taggedText = text;
     taggedText = taggedText.replace(/\n/g, " "); //swap all endlines by spaces
@@ -58,8 +103,9 @@ export class AbstractProcessingService {
 
     for (let acronym of acronymList) {
       // transform `longform (shortform)` strings into `shortform`
-      taggedText = taggedText.split(acronym.longform + " (" + acronym.shortform + ")").join(acronym.shortform);
-      taggedText = taggedText.split(acronym.longform + "(" + acronym.shortform + ")").join(acronym.shortform);
+      taggedText = taggedText.split(`(?i)${acronym.longform + " (" + acronym.shortform + ")"}`).join(acronym.shortform);
+      console.log(taggedText.split(`(?i)${acronym.longform + " (" + acronym.shortform + ")"}`).join(acronym.shortform));
+      taggedText = taggedText.split(`(?i)${acronym.longform + "(" + acronym.shortform + ")"}`).join(acronym.shortform);
       // transform `shortform (longform)` string into `shortform`
       taggedText = taggedText.split(acronym.shortform + " (" + acronym.longform + ")").join(acronym.shortform);
       taggedText = taggedText.split(acronym.shortform + "(" + acronym.longform + ")").join(acronym.shortform);
@@ -71,11 +117,5 @@ export class AbstractProcessingService {
     }
 
     return taggedText;
-  }
-
-  //replace all occurences of <find> with <replace> in <str>
-  public replaceAll(str, find: string, replace: string) {
-    //console.log(`Replacing ${find} with ${replace}`);
-    return str.replace(new RegExp(`\\b${find}\\b`, 'gi'), replace);
   }
 }
