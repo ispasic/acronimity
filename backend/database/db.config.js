9d9ebcc1560aab7dc5618d427eb8be30768d53cf
Maxim Filimonov <max.filimonov@gmail.com>
2021-11-09 17:54:20 +0000

    env user info for mongodb

--- backend/database/db.config.js
+++ backend/database/db.config.js
@@ -1,3 +1,8 @@
+// read environment variables from .env file
+require('dotenv').config({ path: './.env' })
+const mongodbUser = process.env.MONGODB_USER;
+const mongodbPassword = process.env.MONGODB_USER_PASSWORD;
+
 module.exports = {
-    urlAcronyms: 'mongodb://acronymsUser:xDw3ZUs5p65KGyDx@localhost:27017/acronyms'
+    urlAcronyms: `mongodb://${mongodbUser}:${mongodbPassword}@localhost:27017/acronyms`
   };
